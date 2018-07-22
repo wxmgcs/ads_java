451156bb996ee6cd588fc8d47c3fd3894f8656cb
Patrick <wxmgcs@sina.cn>
2018-07-22 23:41:15 +0800

    链表练习题

---
+++ src/main/java/cn/diyai/linklist/exercise/RandomListNode.java
@@ -0,0 +1,11 @@
+package cn.diyai.linklist.exercise;
+
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
