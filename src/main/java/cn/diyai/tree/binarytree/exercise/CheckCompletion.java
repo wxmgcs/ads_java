cdd42822ec67dbf9c1013e4305fec62f34a08dc7
Patrick <wxmgcs@sina.cn>
2018-07-23 00:00:24 +0800

    二叉树 练习题

---
+++ src/main/java/cn/diyai/tree/binarytree/exercise/CheckCompletion.java
@@ -0,0 +1,43 @@
+package cn.diyai.tree.binarytree.exercise;
+
+/**
+ * Created by xing on 4/30/17.
+ * 有一棵二叉树,请设计一个算法判断它是否是完全二叉树。
+
+ 给定二叉树的根结点root，请返回一个bool值代表它是否为完全二叉树。树的结点个数小于等于500。
+ */
+import cn.diyai.tree.TreeNode;
+
+import java.util.*;
+
+public class CheckCompletion {
+    public boolean chk(TreeNode root) {
+        // write code here
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        boolean flag = false; // 表征前面的节点是否已经是叶子节点
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- != 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    if (flag == true) {
+                        return false;
+                    }
+                    queue.offer(node.left);
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    } else {
+                        flag = true;
+                    }
+                } else if (node.right != null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
