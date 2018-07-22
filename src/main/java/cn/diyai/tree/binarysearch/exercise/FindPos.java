698beede15482c28e7dea23a933be19f5efdb43b
Patrick <wxmgcs@sina.cn>
2018-07-23 00:00:38 +0800

    二叉树 练习题

---
+++ src/main/java/cn/diyai/tree/binarysearch/exercise/FindPos.java
@@ -0,0 +1,33 @@
+package cn.diyai.tree.binarysearch.exercise;
+
+/**
+ * 有一个有序数组arr，其中不含有重复元素，请找到满足arr[i]==i条件的最左的位置。如果所有位置上的数都不满足条件，返回-1。
+
+ 给定有序数组arr及它的大小n，请返回所求值。
+
+ 测试样例：
+ [-1,0,2,3],4
+ 返回：2
+ */
+
+public class FindPos {
+    public int findPos(int[] arr, int n) {
+        // write code here
+        int lo = 0;
+        int hi = arr.length - 1;
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] >= mid) {
+                hi = mid;
+            } else {
+                lo = mid;
+            }
+        }
+        if (arr[lo] == lo) {
+            return lo;
+        } else if (arr[hi] == hi) {
+            return hi;
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/cn/diyai/tree/binarysearch/exercise/TreeNode.java
@@ -0,0 +1,10 @@
+package cn.diyai.tree.binarysearch.exercise;
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
